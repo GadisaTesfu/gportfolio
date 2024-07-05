85f3fee9ca9533d4ddb8c8b66d9132462e1214af
Gadisa Tesfu <gadisatesfu@mail.siinqeebank.com>
2024-07-05 16:23:05 +0300

    Fix accessibility issues and update CSS animations

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import Intro from './components/body/Intro';
+import Header from './components/header/Header';
+import Skill from './components/body/Skill';
+import Project from './components/body/Project';
+import Footer from './components/footer/Footer';
 import './App.css';
+import './style.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Intro />} />
+        <Route path="/intro" element={<Intro />} />
+        <Route path="/skill" element={<Skill />} />
+        <Route path="/project" element={<Project />} />
+      </Routes>
+      <Footer />
+    </Router>
   );
 }
 
